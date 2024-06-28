d4c6282aae4b48c7e285db12d67123d82715d64e
Matheus216 <theusgts@gmail.com>
2024-06-27 21:28:45 -0300

    new features

---
+++ src/nobel.console.test/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Hello, Nobel!");
+
+var builder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+
+var services = new SampleAPIService(builder.Build());
+
+try {
+   
+   await services.GetDistinctTopicsAsync();
+   services.FillTopics();
+   
+   foreach (var topic in services.Topics)
+   {
+      Console.WriteLine(topic);
+   }
+
+   Console.WriteLine($"End process, find: {services?.Topics.Count} Topics");
+
+} catch (Exception e)
+{
+   Console.WriteLine(e.Message);
+}
